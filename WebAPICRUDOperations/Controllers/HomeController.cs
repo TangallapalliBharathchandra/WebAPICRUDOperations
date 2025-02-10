213b79f1c90dab5841f8144f1a6bc07f7986cdb6
bharath chandra tangallapalli <bharathchandra0123@gmail.com>
2025-02-09 23:52:42 -0600

    Revert "fixed the review commnets"
    This reverts commit de79ec19d334a3bf47d17debab54df7e1d3c4828.

--- WebAPICRUDOperations/Controllers/HomeController.cs
+++ WebAPICRUDOperations/Controllers/HomeController.cs
@@ -7,9 +7,5 @@ namespace WebAPICRUDOperations.Controllers
     [ApiController]
     public class HomeController : ControllerBase
     {
-        public IActionResult Index()
-        {
-            return Ok("Welcome to Web API CRUD Operations");
-        }
     }
 }
